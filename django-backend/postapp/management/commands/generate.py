a2615880615e76b550613a138e9a665e91386cce
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-07-29 12:41:14 -0500

    created manangement command to allow user to generate secret key

---
+++ django-backend/postapp/management/commands/generate.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.core.management.utils import get_random_secret_key
+
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    new_secret_key = get_random_secret_key()
+
+    with open('./djangobackend/.env', 'w') as f:
+      f.write(f"SECRET_KEY='{new_secret_key}'")
