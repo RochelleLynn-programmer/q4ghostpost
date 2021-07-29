5f0d18aa144c8b9f298e5c4d207a5c91c6ce2905
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-07-29 11:59:44 -0500

    removed secret key from settings, going to write a script that generates a new one for presentation of project

--- django-backend/djangobackend/settings.py
+++ django-backend/djangobackend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^q-m_#(ehz*%@4@7am1yvze&!kqw=849wra*i%04v5k4ne42$'
+SECRET_KEY=os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
