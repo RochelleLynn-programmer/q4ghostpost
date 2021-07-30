c349348d692d226dd21dded0e644ced84c5db79b
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-07-29 19:50:02 -0500

    modified generate to run a subprocess to unstage the .env file once a new django secret key is generated

--- django-backend/postapp/management/commands/generate.py
+++ django-backend/postapp/management/commands/generate.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.core.management.utils import get_random_secret_key
-
+import subprocess
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
@@ -10,3 +10,6 @@ class Command(BaseCommand):
       f.write(f"SECRET_KEY='{new_secret_key}'")
     with open('../.gitignore', 'a') as f:
       f.write('.env')
+
+    subprocess.run(['git', 'rm', '-r', '--cached', 'djangobackend/.env'])
+    subprocess.run(['python', 'manage.py', 'migrate'])
